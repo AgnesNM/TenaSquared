ceec98c8fbdbe2df4caff5e83c9b5ba483a05077
AgnesNM <agnesndutamwaniki@gmail.com>
2023-09-16 01:37:45 +0300

    create models and register

--- TenaSquaredApp/models.py
+++ TenaSquaredApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    cust_name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.cust_name
